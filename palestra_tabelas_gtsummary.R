de6e11cb794c5384b0371e8d040aa2c7c4b4dbb7
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-06-11 22:38:33 -0300

    Add files via upload

---
+++ palestra_tabelas_gtsummary.R
@@ -0,0 +1,37 @@
+### R-Ladies Gyn
+### Palestra: gtsummary: uma nova maneira de criar tabelas elegantes e flexíveis
+### Palestrante: Quemuel Rodrigues (Baruque)
+
+# Conteúdo ---------------------------------------------------------------------------------------------------------------------------------
+
+# 1 - Entendendo o que é o {gt};
+# 2 - Aprendendo os verbos do {gt};
+# 3 - Construindo tabelas no R utilizando o {gt};
+# 4 - Exportando tabelas com o {gt};
+# 5 - Compreendendo o que é o {gtExtras};
+# 6 - Produzindo lindas tabelas com o {gtExtras};
+# 7 - Inserindo plots dentro de tabelas;
+# 8 - Personalizando tabelas com imagens e logos.
+
+# Verbos (camadas) das tabelas -------------------------------------------------------------------------------------------------------------
+
+# - Títulos
+# - Subtítulos
+# - Rótulos das linhas
+# - Rótulos das colunas
+# - Células
+
+# Instalar pacotes -------------------------------------------------------------------------------------------------------------------------
+
+install.packages("gt") # Conhecido por ser o ggplot das tabelas.
+install.packages("gtsummary")
+install.packages("devtools")
+devtools::install_github("BaruqueRodrigues/renda.brasileirao")
+
+# Fluxo de trabalho no {gt} ----------------------------------------------------------------------------------------------------------------
+
+library(dplyr)
+library(gt)
+library(rendas.brasileirao)
+
+dados <- renda.brasileirao::
